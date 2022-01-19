319daa6a87caa6ebc6ae968f7ee0b0d3c69a1535
Owen Roberts <roberts.7@icloud.com>
2022-01-19 09:55:48 -0800

    x86 / x86_64 support

--- build/common.mk
+++ build/common.mk
@@ -50,7 +50,8 @@ common-patch:
 	&& patch -p1 < $(PATCH_DIR)/nacl_armv6_2.patch \
 	&& patch -p2 < $(PATCH_DIR)/4k.patch \
 	&& patch -p2 < $(PATCH_DIR)/macos_h264_encoder.patch \
-	&& patch -p2 < $(PATCH_DIR)/fix_python_used_by_gn.patch
+	&& patch -p2 < $(PATCH_DIR)/disable_use_hermetic_xcode_on_linux.patch \
+	&& patch -p2 < $(PATCH_DIR)/add_licenses.patch
 
 .PHONY: common-package
 common-package: copy
@@ -59,7 +60,7 @@ common-package: copy
 
 .PHONY: generate-licenses
 generate-licenses:
-	vpython3 $(SRC_DIR)/tools_webrtc/libs/generate_licenses.py --target :webrtc $(BUILD_DIR) $(BUILD_DIR)
+	python3 $(SRC_DIR)/tools_webrtc/libs/generate_licenses.py --target :webrtc $(BUILD_DIR) $(BUILD_DIR)
 
 .PHONY: common-copy
 common-copy: generate-licenses
