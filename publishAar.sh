6cd1f1d1abc8257f4775e6e9a9a2b0ab5a5c8690
Owen Roberts <roberts.7@icloud.com>
2023-09-25 18:33:26 -0600

    m106 fork

--- publishAar.sh
+++ publishAar.sh
@@ -8,7 +8,7 @@ while IFS="=" read -r key value; do
     esac
   done < ./VERSION
 
-AAR_URL=https://github.com/crow-misia/libwebrtc-bin/releases/download/${VERSION}/libwebrtc-android.tar.xz
+AAR_URL=https://github.com/ow-ro/libwebrtc-bin/releases/download/${VERSION}/libwebrtc-android.tar.xz
 
 echo AAR_URL=${AAR_URL}
 
@@ -22,6 +22,6 @@ mvn install:install-file \
     -Dfile=aar/libwebrtc.aar \
     -Dpackaging=aar \
     -Dversion=${VERSION} \
-    -DgroupId=com.github.crow-misia \
+    -DgroupId=com.github.ow-ro \
     -DartifactId=libwebrtc-bin
 
